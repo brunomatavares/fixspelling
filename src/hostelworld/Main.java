5bba1ca7a8f355354b547d5c90ee27d3fc04d320
btavares <brunomatavares@gmail.com>
2017-12-21 18:22:00 +0000

    Fix #1 fix spelling

---
+++ src/hostelworld/Main.java
@@ -0,0 +1,19 @@
+package hostelworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(fixSpelling("hostleworld"));
+	}
+
+	public static String fixSpelling(String name) {
+		String wordToCheck = new String(name);		
+		if ("hostleworld".equals(wordToCheck)) {
+			name = "hostelworld";
+		} else {
+			fixSpelling(name);
+		}
+		return name;
+	}
+
+}
